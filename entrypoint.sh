6e36fd0d092b33bc7b7adef8de6eafe8120c64c1
Avishai <avishaik@nimbleway.com>
2024-12-23 14:22:48 +0200

    change exclude

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 json_array='[]'
-EXCLUDE_FILES=${EXCLUDE_FILES:-""}
+EXCLUDE_FILES=${EXCLUDE_FILES:-"^$"}
 
 while IFS= read -r file; do
-    if echo "$EXCLUDE_FILES" | grep -qw "$(basename "$file")"; then
-        continue
-    fi
-
     deployment=""
     deployment=$(cat "$file" | yq '. | select(.kind == "Deployment") | .metadata.name')
     json_array=$(jq --arg file "$file" --arg deployment "$deployment" '. += [{"file": $file, "deployment": $deployment}]' <<< "$json_array")
-done < <(ls $DIRECTORY)
+done < <(ls $DIRECTORY | grep -v -E "$EXCLUDE_FILES")
 
 echo "$json_array"
 echo "deployments=$(echo $json_array)" >> $GITHUB_OUTPUT
