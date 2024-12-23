73043bee7617da6bc187edebb5d7db6dace5c23d
Avishai <avishaik@nimbleway.com>
2024-12-23 15:43:59 +0200

    wrong name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 json_array='[]'
-EXCLUDE_FILES=${EXCLUDE_FILES:-"^$"}
+EXCLUDE=${EXCLUDE:-"^$"}
 
 while IFS= read -r file; do
     deployment=""
     deployment=$(cat "$file" | yq '. | select(.kind == "Deployment") | .metadata.name')
     json_array=$(jq --arg file "$file" --arg deployment "$deployment" '. += [{"file": $file, "deployment": $deployment}]' <<< "$json_array")
-done < <(ls $DIRECTORY | grep -v -E "$EXCLUDE_FILES")
+done < <(ls $DIRECTORY | grep -v -E "$EXCLUDE")
 
 echo "$json_array"
 echo "deployments=$(echo $json_array)" >> $GITHUB_OUTPUT
